c8ad99fe75382c11ddad2af15eb10e31291b5f06
김태훈 <hyri0510@gmail.com>
2023-03-09 17:49:34 +0900

    김태훈 : 헤더, 진입점, 변수 추가

--- CPP/HotkeyMemo/HotkeyMemo.cpp
+++ CPP/HotkeyMemo/HotkeyMemo.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 
-/*
+/* 
 * === 프로젝트/파일/창 생성 및 열기 ===
 * 새 프로젝트 생성하기 Ctrl + Shift + N
 * 새 파일 추가하기 Ctrl + Shift + A
@@ -8,7 +8,7 @@
 * 기존 프로젝트 열기 Ctrl + Shift + O
 * 기존 파일 열기 Ctrl + O
 * 솔루션 탐색기 열기 Ctrl + Alt + L
-*
+* 
 * === 편집관련 단축키 Editing ===
 * 자동완성 Ctrl + Space
 * 자동 정렬하기 Ctrl + K + F
@@ -21,7 +21,7 @@
 * 소문자변환 Ctrl + U
 * 대문자변환 Ctrl + Shift + U
 * 함수 접고 펴기 Ctrl + M + M
-*
+* 
 * === 커서 이동 관련 단축키 ===
 * 현재 행에서 맨 앞으로 이동 Home
 * 현재 행에서 맨 뒤로 이동 End
@@ -33,26 +33,26 @@
 * 괄호 끝으로 이동 Ctrl + ]
 * 괄호 범위 드래그 Ctrl + Shift + ]
 * 줄 상하로 이동시키기 Alt + ↑ / ↓
-*
+* 
 * === 파일 이동 / 엿보기 관련 ===
 * 함수 정의문으로 바로 이동 F12
 * 함수 정의문 엿보기 Alt + F12
 * 파일로 이동 Ctrl + Shift + G
-*
+* 
 * === 검색 / 찾기 / 바꾸기 ===
-* 찾기 Ctrl + F
+* 찾기 Ctrl + F 
 * 바꾸기 Ctrl + H
 * 이름 일괄 변경 Ctrl + R + R
-*
+* 
 * === 빌드 / 컴파일 / 디버깅 관련 단축키 ===
 * 솔루션 빌드 Ctrl + Shift + B
 * 조사식 확인하기 Shift + F9
-*
+* 
 * == 그 외 단축키 ===
 * 모든 참조 찾기 Shift + F12
 * 헤더파일 자동 삽입 Alt + Enter / Ctrl + .
 * 코드 감싸기 Ctrl + K + S
-*
+* 
  */
 
 int main()
