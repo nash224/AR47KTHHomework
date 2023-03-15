5d6651a92ff50081282730a50110ee1e09a621ba
김태훈 <hyri0510@gmail.com>
2023-03-15 13:14:06 +0900

    김태훈 : 3/14 unsigned, char, printf, const, inline project 수정

--- CPP/017_Printf/017_Printf.cpp
+++ CPP/017_Printf/017_Printf.cpp
@@ -8,7 +8,8 @@ int main()
 
     char Arr[5] = { '1','2','3','4',0 };
 
-    long long value01 = Arr;
+    //  long long value01 = Arr;
+    //  char value02 = Arr;
     //  자료형이 다르기 때문에 대입이 안된다.
 
     int value0 = Arr[0];
@@ -16,7 +17,11 @@ int main()
     int value2 = Arr[2];
     int value3 = Arr[3];
     int value4 = Arr[4];
-    int value5 = Arr[5];
+    int value5 = Arr[5];    // 배열의 끝에 0 대신 Arr[5]를 집적 넣어주었다.
 
     printf_s(Arr);
 }
+
+//  === 핵심요약 ===
+
+//  printf는 콘솔창에 글자를 출력한다.
