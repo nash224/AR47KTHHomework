2c0bdd90f2f49b65176812403b836242f76cfc05
김태훈 <hyri0510@gmail.com>
2023-04-24 17:27:16 +0900

    김태훈 : 04-24 TypeDefEx, MainParameter 추가

---
+++ CPP/079_MainParameter/079_MainParameter.cpp
@@ -0,0 +1,24 @@
+﻿// 079_MainParameter.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+int main(int _Count, const char** _InitText)
+{
+	// CWindow NewWindow = new CWindow();
+	// NewWindow.Open();
+	// 
+	// 
+
+	// 내 프로세스가 다른 프로세스를 실행할수도 있습니다.
+	// 내가 실행하는 곳에 따라서 위치가 달라진다.
+	int Result = system("C:\\C\\AR47KTHHomework\\CPP\\x64\\Debug\\078_BitTest.exe");
+
+	for (size_t i = 0; i < _Count; i++)
+	{
+		// char*
+		std::cout << _InitText[i] << std::endl;
+	}
+
+	return 1;
+}
---
+++ CPP/080_TypeDefEx/080_TypeDefEx.cpp
@@ -0,0 +1,26 @@
+﻿// 080_TypeDefEx.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+
+typedef int MYINT, * MYTEST;
+
+typedef wchar_t WCHAR, * HAMBERGER, * PIZZA, ** CHICKEN;
+
+int main()
+{
+    int* Ptr0 = nullptr;
+    MYTEST Ptr1 = nullptr;
+
+    HAMBERGER Ptr2 = nullptr;
+
+    HAMBERGER Arr[5] = {};
+
+    CHICKEN PPtr = Arr;
+
+    
+
+    // wchar_t* LPWSTR;
+
+    std::cout << "Hello World!\n";
+}
